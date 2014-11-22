98c50e85c9955c4d0f5a674d2cafeea3ca7232c2
Aaron Spears <aaronjs0@gmail.com>
2014-11-22 11:29:23 -0800

    new controllers for progress and home page

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,9 +1,12 @@
 from app import app, db
+from app.deco import login_required
 from flask import render_template
 
 @app.route('/home')
+@login_required()
 def home():
-    return render_template('home.html')
+    user = g.user
+    return render_template('home.html', user = user)
 
 @app.route('/')
 def index():
--- app/controllers/progress.py
+++ app/controllers/progress.py
@@ -1,6 +1,9 @@
 from app import app, db
+from app.deco import login_required
 from flask import render_template
 
 @app.route('/progress')
+@login_required()
 def render():
-    return render_template('progress.html')
+    user = g.user
+    return render_template('progress.html', user = user)
