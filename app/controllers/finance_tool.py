98a079a6f640f9fcf0f7776fe7af09413537443a
J Delaney <j.delaney.sd@gmail.com>
2014-11-22 06:33:08 -0800

    Daily input tool

--- app/controllers/finance_tool.py
+++ app/controllers/finance_tool.py
@@ -1,4 +1,5 @@
 from flask import render_template_string, render_template, request, url_for, redirect, g
+from datetime import datetime
 from app import app, db
 from app.deco import login_required
 
@@ -36,10 +37,62 @@ def tool_setup_goals():
             goals[goal['category']] = goal['amount']
     return render_template("tool/goals.html", goals=goals, err=err)
 
-@app.route('/tool/input-daily')
+@app.route('/tool/daily', methods=['GET', 'POST'])
 @login_required
-def tool_input_daily():
-    return render_template_string("Hello")
+def tool_daily():
+    user = g.user
+    err = None
+
+    # Why? Because I can
+    today = datetime.today()
+    today = datetime(today.year, today.month, today.day)
+
+    if request.method == 'POST':
+        i = -1
+        if 'entries' in user:
+            for index, entry in enumerate(user.entries):
+                if entry['date'] == today:
+                    i = index
+        else:
+            user['entries'] = [{}]
+            i = 0
+        if i == -1:
+            user.entries.append({})
+            i = len(user.entries) - 1
+
+        user.entries[i] = {'date': today, 'spending': {}, 'saving': {}}
+
+        for category, value in request.form.iteritems():
+            category = unicode(category)
+            value = value.strip()
+            try:
+                value = int(value)
+            except ValueError:
+                value = 0
+                err = 'You may only use numerical values'
+
+            m = 'spending'
+            if category in ['car', 'college']:
+                m = 'saving'
+            user.entries[i][m][category] = value
+        user.save()
+
+        if not err:
+            return redirect(url_for('tool_main'))
+
+
+    daily = {}
+    if 'entries' in user:
+        for entry in user.entries:
+            if entry['date'] == today:
+                daily = entry
+    if 'spending' not in daily:
+        daily['spending'] = {}
+    if 'saving' not in daily:
+        daily['saving'] = {}
+
+
+    return render_template("tool/daily.html", daily=daily, err=err)
 
 @app.route('/tool/view-stats')
 @login_required
