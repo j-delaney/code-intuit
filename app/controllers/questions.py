aa2511859f27cf7ed1452c281e1c0324d8a7af6d
Aaron Spears <aaronjs0@gmail.com>
2014-11-22 07:37:40 -0800

    updated controller for questions

--- app/controllers/questions.py
+++ app/controllers/questions.py
@@ -13,15 +13,16 @@ def render_answers(article_name):
     if request.method == 'POST':
         num_correct = 0
         success = False
+        controller_return = True
 
         for question_id, correct in request.form.iteritems():
             if correct == True or correct == 'True' or correct == u'True'
-            num_correct+=1
+                num_correct+=1
       
         if num_correct == 3
             success = True
       
-        return render_template('questions.html', num_correct, success)
+        return render_template('questions.html', num_correct, success, controller_return)
 
     else:
         return render_template('questions.html') 
