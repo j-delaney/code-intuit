e886df57a8a6f9b5ee5b215a7146b03bbb3c655f
J Delaney <j.delaney.sd@gmail.com>
2014-11-22 03:13:11 -0800

    Basic goal data

--- app/controllers/finance_tool.py
+++ app/controllers/finance_tool.py
@@ -1,4 +1,4 @@
-from flask import render_template_string, render_template
+from flask import render_template_string, render_template, request, url_for, redirect, g
 from app import app, db
 from app.deco import login_required
 
@@ -8,10 +8,27 @@ from app.deco import login_required
 def tool_main():
     return render_template('tool/main.html')
 
-@app.route('/tool/setup-goals')
+@app.route('/tool/setup-goals', methods=['GET', 'POST'])
 @login_required
 def tool_setup_goals():
-    return render_template_string("Hello")
+    user = g.user
+    err = None
+
+    if request.method == 'POST':
+        user.goals = []
+
+        for category, value in request.form.iteritems():
+            value = value.strip()
+            if value != u'':
+                try:
+                    value = int(value)
+                    user.goals.append({'category': category, 'amount': value})
+                except ValueError:
+                    err = 'You may only use numerical values'
+        user.save()
+        if not err:
+            return redirect(url_for('tool_main'))
+    return render_template("tool/goals.html", user=user, err=err)
 
 @app.route('/tool/input-daily')
 @login_required
