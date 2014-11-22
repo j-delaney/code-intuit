acfab5320084589fa4e6185cbe027e2dda58207c
J Delaney <j.delaney.sd@gmail.com>
2014-11-21 21:50:51 -0800

    Ability to remove all documents and fill in basic users

--- app/__init__.py
+++ app/__init__.py
@@ -7,5 +7,6 @@ app.secret_key = 'ajdfshasljbsdb6546s4b365s4dfb31dsz3b4143iyt5q387*(*^&*&%$&^$*&
 
 db = Connection()
 from models.user import User
+from models.question import Question
 
 from app.controllers import *
